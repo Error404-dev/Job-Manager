a37cc77ad43e65c7bba7311da66f86e48ea58ecf
Error404-dev <74318351+Error404-dev@users.noreply.github.com>
2022-07-14 19:55:03 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,30 +1,20 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-import {
-  BadRequestError,
-  NotFoundError,
-  UnAuthenticatedError,
-} from "../errors/index.js";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
 
 const register = async (req, res) => {
-  //   try {
-  //     const user = await User.create(req.body);
-  //     res.status(201).json({ user });
-  //   } catch (error) {
-  //     // res.status(500).json({ msg: "there was an error" });
-  //     next(error); //next pass error on next middleware(errorhandler.js)
-  //   }
   const { name, email, password } = req.body;
+
   if (!name || !email || !password) {
-    throw new customAPIError("please provide all values");
+    throw new BadRequestError("please provide all values");
   }
   const userAlreadyExists = await User.findOne({ email });
   if (userAlreadyExists) {
     throw new BadRequestError("Email already in use");
   }
   const user = await User.create({ name, email, password });
+
   const token = user.createJWT();
-  // const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({
     user: {
       email: user.email,
@@ -36,7 +26,6 @@ const register = async (req, res) => {
     location: user.location,
   });
 };
-
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -46,33 +35,32 @@ const login = async (req, res) => {
   if (!user) {
     throw new UnAuthenticatedError("Invalid Credentials");
   }
-  console.log(user);
+
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
     throw new UnAuthenticatedError("Invalid Credentials");
   }
   const token = user.createJWT();
-  user.password = undefined; //so that we dont send password back
-  // res.send("login user");
+  user.password = undefined;
   res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
-
 const updateUser = async (req, res) => {
   const { email, name, lastName, location } = req.body;
   if (!email || !name || !lastName || !location) {
     throw new BadRequestError("Please provide all values");
   }
   const user = await User.findOne({ _id: req.user.userId });
+
   user.email = email;
   user.name = name;
   user.lastName = lastName;
   user.location = location;
 
   await user.save();
+
   const token = user.createJWT();
-  // console.log(req.user);
+
   res.status(StatusCodes.OK).json({ user, token, location: user.location });
-  // res.send("updateUser");
 };
 
 export { register, login, updateUser };
